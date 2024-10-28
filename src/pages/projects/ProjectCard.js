d9fefb551fae26b53230a225e2a8813f66f565ee
Kavindu <kavindu2308@gmail.com>
2024-10-28 23:20:54 +0530

    Updated the project cards

--- src/pages/projects/ProjectCard.js
+++ src/pages/projects/ProjectCard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const ProjectCard = ({ title, des, src, icons, link }) => {
+const ProjectCard = ({ title, des, src, techStack, link }) => {
     return (
         <div className="w-full p-4 xl:px-12 h-auto xl:py-10 rounded-lg shadow-shadowOne flex flex-col bg-gradient-to-r from-bodyColor to-[#202327] group hover:bg-gradient-to-b hover:from-gray-900 hover:gray-900 transition-colors duration-1000">
             <div className="w-full h-[80%] overflow-hidden rounded-lg">
@@ -24,7 +24,7 @@ const ProjectCard = ({ title, des, src, icons, link }) => {
                         {des}
                     </p>
                 </div>
-                <div className='mt-5 font-bold'>
+                {/* <div className='mt-5 font-bold'>
                     <h1>Tech Stack</h1>
                     <div className="flex  mt-3 gap-4">
                         {icons.map((Icon, index) => (
@@ -33,7 +33,17 @@ const ProjectCard = ({ title, des, src, icons, link }) => {
                             </span>
                         ))}
                     </div>
-                </div>
+                </div> */}
+                <div className="flex space-x-2 mt-4 font-['Inter'] text-xs">
+                {techStack.map((tech) => (
+                  <span
+                    key={tech}
+                    className="px-2 py-1 border-[1px] border-designColor text-white rounded-md  "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
 
             </div>
         </div>
--- src/pages/projects/Projects.js
+++ src/pages/projects/Projects.js
@@ -1,7 +1,4 @@
 import React from 'react'
-import { FaCss3Alt, FaFigma, FaHtml5, FaJs, FaNodeJs, FaReact } from 'react-icons/fa'
-import { GrMysql } from 'react-icons/gr'
-import { SiMongodb, SiSpringboot } from 'react-icons/si'
 import { projectsC1, projectsC2, projectsC3 } from '../../assests'
 import { PageTitle } from '../../components/PageTitle/PageTitle'
 import ProjectCard from './ProjectCard'
@@ -17,7 +14,7 @@ const Projects = () => {
                     title="Hotel Reservation System"
                     des='An online hotel reservation system with a user-friendly interface, admin dashboard, role-based access control, and room filtering capabilities.'
                     src={projectsC1}
-                    icons={[SiSpringboot, FaReact, GrMysql, FaFigma]}
+                    techStack = {["Spring Boot", "React.js", "MySQL", "Figma"]}
                     link="https://github.com/kavindu-gamage/Hotel-Reservation-System"
                 />
                 <ProjectCard
@@ -25,14 +22,16 @@ const Projects = () => {
                     des='A user-friendly E-Commerce web application with essential features like user profiles,
                     shopping carts, payment methods, and exploring products.'
                     src={projectsC2}
-                    icons={[FaNodeJs, FaReact, SiMongodb]}
+                    techStack = {["Express.js", "Node.js", "React.js", "MongoDb"]}
+                    // icons={[FaNodeJs, FaReact, SiMongodb]}
                     link="https://github.com/kavindu-gamage/e-commerce-web-back-end"
                 />
                 <ProjectCard
                     title="Burger Shop Website"
                     des='A responsive one-page website for a burger shop, featuring sections for Home, About, Menu, Offers, and Locations, providing customers with an enticing online experience'
                     src={projectsC3}
-                    icons={[FaHtml5 ,FaCss3Alt, FaJs]}
+                    techStack = {["HTML", "CSS", "Java Script"]}
+                    // icons={[FaHtml5 ,FaCss3Alt, FaJs]}
                     link="https://github.com/kavindu-gamage/burgerShop-landingPage"
                 />
             </div>
